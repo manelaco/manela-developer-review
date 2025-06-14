b10c9f693e702170d8820cdf6ab7512eb4f22a35
Kim <manela.cie@gmail.com>
2025-06-14 11:34:20 -0400

    chore: update gitignore and remove .next from tracking

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oEIJT7Iom/RmhH6JkxHRtl6FNL8vKsqjRW+BfBrVs1U=\"\n}"
--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/employee/portal":["static\u002Fchunks\u002Fpages\u002Femployee\u002Fportal.js"],sortedPages:["\u002F_app","\u002F_error","\u002Femployee\u002Fportal"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
